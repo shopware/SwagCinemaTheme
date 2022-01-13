59bbc03729be4c9b5df0adb5cf31f3c113c27702
Thu.Vo <thu.vo@shopware.com>
2022-01-13 16:32:48 +0700

    TCM-57 - Customized Products

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,7 +1,17 @@
 import BackButtonPlugin from './plugin/product-detail/back-button';
 import CinemaListingPlugin from './plugin/listing/cinema-listing.plugin';
+import CustomizedProductsStepByStepWizard from "./plugin/customized-product/customized-product-step.plugin";
+import CinemaCustomizedProductPriceDisplay from "./plugin/customized-product/customized-products-price-display.plugin";
 
 const PluginManager = window.PluginManager;
 
 PluginManager.register('BackButton', BackButtonPlugin, '[data-back-button]');
 PluginManager.override('Listing', CinemaListingPlugin, '[data-listing]');
+
+if (PluginManager.getPluginList().SwagCustomizedProductsStepByStepWizard) {
+    PluginManager.override('SwagCustomizedProductsStepByStepWizard', CustomizedProductsStepByStepWizard, '*[data-swag-customized-product-step-by-step="true"]');
+}
+
+if (PluginManager.getPluginList().SwagCustomizedProductPriceDisplay) {
+    PluginManager.override('SwagCustomizedProductPriceDisplay', CinemaCustomizedProductPriceDisplay, '[data-swag-customized-product-price-display="true"]',);
+}
