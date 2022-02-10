ec26be7384d52ff83f12e34329614ec50db7c4c3
Thu.Vo <thu.vo@shopware.com>
2022-02-10 16:20:09 +0700

    TCM-113 - Rating in filter modal needs alignment

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,5 +1,6 @@
 import BackButtonPlugin from './plugin/product-detail/back-button';
 import CinemaListingPlugin from './plugin/listing/cinema-listing.plugin';
+import CinemaFilterRatingSelectPlugin from './plugin/listing/cinema-filter-rating-select.plugin';
 import CustomizedProductsStepByStepWizard from "./plugin/customized-product/customized-product-step.plugin";
 import CinemaCustomizedProductPriceDisplay from "./plugin/customized-product/customized-products-price-display.plugin";
 import CinemaZoomModalPlugin from './plugin/zoom-modal/zoom-modal.plugin';
@@ -8,6 +9,7 @@ const PluginManager = window.PluginManager;
 
 PluginManager.register('BackButton', BackButtonPlugin, '[data-back-button]');
 PluginManager.override('Listing', CinemaListingPlugin, '[data-listing]');
+PluginManager.override('FilterRatingSelect', CinemaFilterRatingSelectPlugin, '[data-filter-rating-select]');
 PluginManager.override('ZoomModal', CinemaZoomModalPlugin, '[data-zoom-modal]');
 
 if (PluginManager.getPluginList().SwagCustomizedProductsStepByStepWizard) {
