8b7a628da7787deec9bdb2e75011ec531e8d75eb
Long <longntran48@gmail.com>
2022-12-06 11:45:04 +0100

    TCM-171 - Make E2E runable on major version

--- Resources/app/storefront/test/e2e/cypress/plugins/index.js
+++ Resources/app/storefront/test/e2e/cypress/plugins/index.js
@@ -2,5 +2,6 @@ require('@babel/register');
 
 module.exports = (on, config) => {
     // eslint-disable-next-line global-require
+    require('cypress-log-to-output').install(on),
     require('cypress-grep/src/plugin')(config);
 };
